932c27d131726a81df2b29f65698b311459d4a03
Alfin-K-Joju <152876085+Alfin-K-Joju@users.noreply.github.com>
2024-09-24 23:56:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide the nav links
+});
+
